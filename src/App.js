dbab2104111b2312eb3bfd2be52312c60baa4d64
MaxRobertsDear <maxhroberts@gmail.com>
2019-07-20 21:25:45 +0100

    Add state to <App />

--- src/App.js
+++ src/App.js
@@ -5,14 +5,19 @@ import UserInput from './UserInput/UserInput';
 import UserOutput from './UserOutput/UserOutput';
 
 class App extends Component {
+  state = {
+    info: [
+      { username: 'MaxRobertsDear', content: 'Time to rule the world' },
+      { username: 'Sarah', content: 'Should have known better' },
+    ]
+  }
 
-  
   render() {
     return (
       <div className="App">
           <UserInput />
-          <UserOutput username='MaxRobertsDear' content='Time to rule the world' />
-          <UserOutput username='MJ' content='Should have known better' />
+          <UserOutput username={this.state.info[0].username} content={this.state.info[0].content} />
+          <UserOutput username={this.state.info[1].username} content={this.state.info[1].content} />
       </div>
     );
   }
