3b24d1b6477a5c991b66ef0c78f7c51048070cc3
MaxRobertsDear <maxhroberts@gmail.com>
2019-07-20 21:36:42 +0100

    Add method to manipulate state

--- src/App.js
+++ src/App.js
@@ -12,9 +12,29 @@ class App extends Component {
     ]
   }
 
+  onClickHandler = (newUserName) => {
+    this.setState({
+      info: [
+        { username: newUserName, content: 'Time to rule the world' },
+        { username: 'Sarah', content: 'This little piggy went all the way home' },
+      ]
+    });
+  }
+
   render() {
+    const style = {
+      backgroundColor: 'white', 
+      font: 'inherit', 
+      border: '1px solid blue', 
+      padding: '8px', 
+      cursor: 'pointer'
+    }
+
     return (
       <div className="App">
+      <button 
+        style={style}
+        onClick={() => this.onClickHandler('YATSI!')} >Change Content</button>
           <UserInput />
           <UserOutput username={this.state.info[0].username} content={this.state.info[0].content} />
           <UserOutput username={this.state.info[1].username} content={this.state.info[1].content} />
